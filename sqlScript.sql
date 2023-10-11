1c6ed2188f9054bdd317141ba8e81653fea6d75f
Ethan Rich <ethanc.rich@gmail.com>
2023-10-11 12:22:00 -0700

    Updated script to include 4 new tables

--- sqlScript.sql
+++ sqlScript.sql
@@ -10,6 +10,32 @@
 -- Now, this was just a testing script. You can just use the following command to get rid the testing table
 -- DROP TABLE car;
 
-CREATE TABLE car (
-    brand VARCHAR(255)
+CREATE TABLE User (
+    user_id INT NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Project (
+    sponsor VARCHAR(255) NOT NULL,
+    project_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (sponsor, project_name)
+);
+
+CREATE TABLE UserProject (
+    user_id INT NOT NULL REFERENCES User(user_id), 
+    project_name VARCHAR(255) REFERENCES Project(project_name),
+    sponsor VARCHAR(255) REFERENCES Project(sponsor),
+    is_team_lead BOOLEAN,
+    PRIMARY KEY (user_id, project_name, sponsor)
+);
+
+CREATE TABLE Transaction (
+    trans_id INT NOT NULL,
+    checkout_datetime DATETIME,
+    expected_return_datetime DATETIME,
+    actual_return_datetime DATETIME,
+    comments VARCHAR(1023),
+    user_id INT NOT NULL REFERENCES User(user_id)
+    PRIMARY KEY (trans_id) 
 );
