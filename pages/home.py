7af4ef08dc9bc7cc84150dce90935cb458ca8c68
Ethan Rich <ethanc.rich@gmail.com>
2023-11-29 21:06:08 -0700

    Initial Commit

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,6 @@
 # home.py
 import streamlit as st
+import psycopg2
 
 #if not logged in dont show anything
 if not st.session_state.get('logged_in', False):
@@ -10,11 +11,35 @@ def logout():
         for key in st.session_state.keys():
             del st.session_state[key]
         st.experimental_rerun()
+
 if st.sidebar.button("Logout"):
-        logout()
+    logout()
 
 st.title("Home Page")
 st.write("Welcome to the Lab Equipment Inventory System!")
 
-
 # TO DO: Add more content for the Home Page here
+
+#Connect to database
+# Define function to connect to your PostgreSQL database
+#Connect to database and close connection on demand
+def connect_to_db():
+    conn = psycopg2.connect(
+        host="bubble.db.elephantsql.com",
+        port="5432",
+        database="ozhptawz",
+        user="ozhptawz",
+        password="rw1e9fAoDhnYmq4D2QKgu51o4g7zS-fJ" #THIS IS UNSAFE. REMOVE THIS IN THE FUTURE.
+    )
+    return conn
+
+def display_available_items():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM equipment WHERE status = 'Available';")
+    rows = cur.fetchall()
+    for row in rows:
+        st.write(row)
+    conn.close()
+
+display_available_items()
